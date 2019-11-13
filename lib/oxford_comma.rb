88b9810cf0a3aef38a2b1a498bd3cddf3788b9e7
Whitney Hodgkins <hodgkins.whitney@gmail.com>
2019-11-13 22:23:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,11 @@
 def oxford_comma(array)
   if array.length < 2 
     array.join
+  elsif array.length == 2 
+    array.join(" and ")
+  elsif array.length == 3
+    array.join(", ")
+    array.insert(-2, and)
   end
 end
 
